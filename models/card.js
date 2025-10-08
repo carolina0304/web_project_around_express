0ce1bbc8896460bf16d032fa464042fab42e7d4d
carolina0304 <garlene122@gmail.com>
2025-10-07 18:28:39 -0600

    card create id

--- models/card.js
+++ models/card.js
@@ -1,34 +1,37 @@
 const mongoose = require("mongoose"); //importa Mongoose.
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String, // este campo se llamara name y guardara el nombre de la tarjeta.
-    required: true, // este campo es obligatorio
-    minlength: 2, // especifica la longitud mínima/máxima de los campos
-    maxlength: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-    validator: {
-      //validador para comprobar que la URL es correcta.
-      validator(v) {
-        return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(
-          v
-        );
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String, // este campo se llamara name y guardara el nombre de la tarjeta.
+      required: true, // este campo es obligatorio
+      minlength: 2, // especifica la longitud mínima/máxima de los campos
+      maxlength: 30,
+    },
+    link: {
+      type: String,
+      required: true,
+      validator: {
+        //validador para comprobar que la URL es correcta.
+        validator(v) {
+          return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(
+            v
+          );
+        },
+        message: "Por favor, introduce una Url imagen válida",
       },
-      message: "Por favor, introduce una Url imagen válida",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId, //Tipo de dato especial para IDs de MongoDB
+      ref: "User", //Referencia al modelo User
+      required: true,
+    },
+    likes: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], //Array de IDs de usuarios que han dado "me gusta"
+      default: [], //Valor por defecto es un array vacío
     },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId, //Tipo de dato especial para IDs de MongoDB
-    ref: "User", //Referencia al modelo User
-    required: true,
-  },
-  likes: {
-    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }], //Array de IDs de usuarios que han dado "me gusta"
-    default: [], //Valor por defecto es un array vacío
-  },
-});
+  { timestamps: true }
+); //timestamps añade automáticamente campos createdAt y updatedAt.
 
 module.exports = mongoose.model("Card", cardSchema);
